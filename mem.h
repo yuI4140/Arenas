535297f2e79a0882d47e2961d75181a4fb98a583
yuI4140 <102521443+yuI4140@users.noreply.github.com>
2023-08-17 04:17:08 +0200

    Update mem.h

--- mem.h
+++ mem.h
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-#include <stdio.h> 
+#include <stdio.h>
 
 typedef struct {
     size_t cap;
@@ -7,7 +7,7 @@ typedef struct {
     void* db;
 } Ren;
 
-void rAssert(int condition, const char* message); 
+void rAssert(int condition, const char* message);
 void rPAssert(void *ptr, const char* message);
 Ren* createRen(size_t cap);
 void destroyRen(Ren* ren);
@@ -18,27 +18,26 @@ void dropDown(Ren* ren);
 void rAssert(int condition, const char* message) {
     if (!condition) {
         fprintf(stderr, "Error: %s\n", message);
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 }
+
 void rPAssert(void* ptr, const char* message) {
     if (!ptr) {
         fprintf(stderr, "Error: %s\n", message);
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 }
+
 Ren* createRen(size_t cap) {
     Ren* ren = (Ren*)malloc(sizeof(Ren));
     rPAssert(ren, "Memory allocation failed for Ren");
-    
+
     ren->cap = cap;
     ren->size = 0;
     ren->db = malloc(cap);
-    if (!ren->db) {
-        free(ren);
-        ren = NULL; 
-        rAssert(0, "Memory allocation failed for Ren database");
-    }
+    rPAssert(ren->db, "Memory allocation failed for Ren");
+
     return ren;
 }
 
@@ -52,7 +51,7 @@ void destroyRen(Ren* ren) {
 void* allocRen(Ren* ren, size_t size) {
     rAssert(ren && size > 0, "Invalid arguments in allocRen");
     rAssert(ren->size + size <= ren->cap, "Memory allocation would exceed capacity");
-    
+
     void* mem = (char*)ren->db + ren->size;
     ren->size += size;
     return mem;
